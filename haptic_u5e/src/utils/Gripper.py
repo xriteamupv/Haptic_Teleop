f9316723e50090cd45b7999cf7410d520a897c2c
Fernando Hernandez <38531693+ferhernandezgobertti@users.noreply.github.com>
2024-05-24 10:46:21 +0200

    Delete haptic_u5e directory

--- haptic_u5e/src/utils/Gripper.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-class Gripper:
-
-    def __init__(self):
-        self.width_limits = [0.0, 100.0] # millimeters
-        self.force_limits = [0.0, 120.0] # Newtons
-        self.grip_detected = False
-        self.current_width = 100.0 # open
-        self.current_force = 0.0
